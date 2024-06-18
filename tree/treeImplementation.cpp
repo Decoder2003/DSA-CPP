6afc53eeb3aabc3e595124185b3545fb6e826290
Decoder2003 <kalradhruv2003@gmail.com>
2024-06-18 09:58:55 +0530

    Day 9

---
+++ tree/treeImplementation.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode{
+    public:
+        int data;
+        TreeNode* left;
+        TreeNode* right;
+
+    TreeNode(int data){
+        this->data = data;
+        this->left = this->right = nullptr;
+    }
+};
+
+class BuildTree{
+    TreeNode* root;
+
+    TreeNode* 
+
+}
+
+
+int main(){
+    return 0;
+}
